fa723e34fc9a77c2da02206eda69748cbab6649e
vinh <dinhvanvinh1001@gmail.com>
2022-05-21 11:16:51 +0700

    create Author_product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ["name", "pub_date", "regis_date", "owner_id"];
+
+    public function Owners()
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function authorProduct()
+    {
+        return $this->hasMany(AuthorProduct::class);
+    }
 }
