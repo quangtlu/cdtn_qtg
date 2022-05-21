070e13aaa0cb00edd076e3a2c986795ddf13692d
Dương Văn Quang <quangbk10@gmail.com>
2022-05-21 10:24:42 +0700

    crud permission

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.admin')
-@section('title', 'Quản lý nhóm quyền')
+@section('title', 'Quản lý quyền')
 @section('content')
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
-        @include('partials.content_header', ['name' => 'nhóm quyền', 'key' => 'Danh sách'])
+        @include('partials.content_header', ['name' => 'quyền', 'key' => 'Danh sách'])
         <!-- /.content-header -->
         <!-- Main content -->
         <div class="content">
@@ -14,29 +14,29 @@
                             <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>Tên nhóm quyền</th>
+                                <th>Tên quyền</th>
                                 <th>Mô tả</th>
                                 <th>Action</th>
                             </tr>
                             </thead>
                             <tbody>
-                                @foreach ($roles as $role)
+                                @foreach ($permissions as $permission)
                                     <tr>
-                                        <td>{{ $role->id }}</td>
-                                        <td>{{ $role->name }}</td>
-                                        <td>{{ $role->description }}</td>
+                                        <td>{{ $permission->id }}</td>
+                                        <td>{{ $permission->name }}</td>
+                                        <td>{{ $permission->description }}</td>
                                         <td>
-                                            <a href="{{ route('admin.roles.edit', ["id" => $role->id]) }}"><button class="btn btn-info btn-sm">Sửa</button></a>
-                                            <a href="{{ route('admin.roles.destroy', ["id" => $role->id]) }}"><button class="btn btn-danger btn-sm">Xóa</button></a>
+                                            <a href="{{ route('admin.permissions.edit', ["id" => $permission->id]) }}"><button class="btn btn-info btn-sm">Sửa</button></a>
+                                            <a href="{{ route('admin.permissions.destroy', ["id" => $permission->id]) }}"><button class="btn btn-danger btn-sm">Xóa</button></a>
                                         </td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
-                        {{ $roles->links() }}
+                        {{ $permissions->links() }}
                     </div>
                     <div class="col-md-12">
-                        <a href="{{ route('admin.roles.create') }}"><button class="btn btn-success float-right m-2">Thêm mới</button></a>
+                        <a href="{{ route('admin.permissions.create') }}"><button class="btn btn-success float-right m-2">Thêm mới</button></a>
                     </div>
                 </div>
                 <!-- /.row -->
