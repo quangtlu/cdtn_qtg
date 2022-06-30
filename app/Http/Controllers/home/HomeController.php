cc1ddd1bb625302b45473e1f82702b4bb20dd59e
Dinh Van Vinh <vinh.dinh@pirago.work>
2022-06-30 12:05:06 +0700

    add data to homepage

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
+use App\Services\PostService;
+use App\Services\ProductService;
 
 class HomeController extends Controller
 {
+    private $postService;
+    private $productService;
+
+    public function __construct(PostService $postService, ProductService $productService)
+    {
+        $this->postService = $postService;
+        $this->productService = $productService;
+    }
+
+
     public function index ()
     {
-        return view('home.index');
+        $posts = $this->postService->getPostHome();
+        $products = $this->productService->getProductHome();
+        return view('home.index', compact('posts', 'products'));
     }
 }
