3cc97d87be0fe82ef48a1186b6b7029a46f32e9e
quang.duong <quang.duong@pirago.work>
2022-07-25 14:04:19 +0700

    update

--- resources/views/home/component/posts/list.blade.php
+++ resources/views/home/component/posts/list.blade.php
@@ -83,5 +83,5 @@
     @endforeach
     {{ $posts->withQueryString()->links() }}
 @else
-    <div class="alert alert-info alert-no-post" style="margin-top: 10px" role="alert">Bài viết đang được cập nhật...</div>
+    <div class="alert alert-warning alert-no-post" style="margin-top: 10px" role="alert">Không có bài viết nào</div>
 @endif
