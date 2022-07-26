a6bdaee5ad431ab95bb587f10459c1c3f5238feb
Vinh Đinh Văn <vinh.dinh@pirago.work>
2022-07-26 17:07:54 +0700

    fix

--- app/Http/Requests/Admin/Product/UpdateProductRequest.php
+++ app/Http/Requests/Admin/Product/UpdateProductRequest.php
@@ -29,7 +29,6 @@ class UpdateProductRequest extends FormRequest
             'regis_date' => 'before:today',
             'author_id' => 'required',
             'description' => 'required',
-            'regis_date' => 'before:today|after:pub_date'
         ];
     }
 
@@ -42,8 +41,6 @@ class UpdateProductRequest extends FormRequest
             'regis_date.before' => 'Ngày đăng kí tác phẩm không được là ngày trong tương lai',
             'author_id.required' => 'Vui lòng chọn tác giả',
             'description.required' => 'Vui lòng nhập miêu tả',
-            'regis_date.before' => 'Ngày xuất bản tác phẩm không được là ngày trong tương lai',
-            'regis_date.after' => 'Ngày đăng kí tác phẩm sau ngày xuất bản',
         ];
     }
 }
